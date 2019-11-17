0b45b0abda0a3f6e9da54cee31057181ae76425a
Ragee <35122830+Ragee23@users.noreply.github.com>
2019-11-17 17:24:15 +0000

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ const currentBlkMan = document.querySelector('#slider-1');
 const previousBtn = document.querySelector('#previousBtn');
 const nextBtn = document.querySelector('#nextBtn');
 let blkMen = ['slider-1.jpg', 'slider-2.jpg', 'slider-3.jpg', 'slider-4.jpg'];
-previousBtn.addEventListener('click', previous);
-nextBtn.addEventListener('click', next);
 
 let num = 0; 
 
@@ -11,16 +9,19 @@ function previous(){
     if (num == 0){
         num = blkMen.length;
     };
-        --num;
-        currentBlkMan.src = blkMen[num];
-        console.log(currentBlkMan); 
-    };
-   
+    --num;
+    currentBlkMan.src = blkMen[num];
+    console.log(currentBlkMan); 
+};
+
 function next(){
     if (num == blkMen.length){
         num = 0;
     };
-        currentBlkMan.src = blkMen[num];
-        num++;
-        console.log(currentBlkMan);      
+    currentBlkMan.src = blkMen[num];
+    num++;
+    console.log(currentBlkMan);      
 };
+
+previousBtn.addEventListener('click', previous);
+nextBtn.addEventListener('click', next);
